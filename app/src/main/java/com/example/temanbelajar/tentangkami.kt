70a26e18bad44ef6ccb2ca40754227fbfc4eec2f
Putri Nuraini <putri_nuraini@teknokrat.ac.id>
2025-06-23 23:09:25 +0700

    menambahkan logika ketika klik kembali maka ke halaman utama

--- app/src/main/java/com/example/temanbelajar/tentangkami.kt
+++ app/src/main/java/com/example/temanbelajar/tentangkami.kt
@@ -1,5 +1,6 @@
-package com.example.temanbelajar
+package com.example.temanbelajar # Sesuaikan dengan package aplikasi Anda
 
+import android.content.Intent // Import Intent untuk navigasi
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,9 @@ class tentangkami : AppCompatActivity() {
         val kembaliIcon: ImageView = findViewById(R.id.kembali_icon)
 
         kembaliIcon.setOnClickListener {
+            // Membuat Intent untuk pindah ke MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent) // Memulai MainActivity
             finish()
         }
     }
