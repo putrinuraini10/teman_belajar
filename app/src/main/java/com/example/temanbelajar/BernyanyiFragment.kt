f7419280a48f7882e152134614bc0919f8fb1a27
Hikmatun Nazilah <hikmatun_nazilah@teknokrat.ac.id>
2025-06-23 20:34:41 +0700

    Mendeklarasi variabel untuk setiap komponen VideoView dan tombol play (satu per video). pada BernyanyiFragment.kt

--- app/src/main/java/com/example/temanbelajar/BernyanyiFragment.kt
+++ app/src/main/java/com/example/temanbelajar/BernyanyiFragment.kt
@@ -1,10 +1,14 @@
 package com.example.temanbelajar
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.VideoView
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -17,8 +21,12 @@ private const val ARG_PARAM2 = "param2"
  */
 class BernyanyiFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var videoView1: VideoView
+    private lateinit var videoView2: VideoView
+    private lateinit var videoView3: VideoView
+    private lateinit var playButton1: ImageView
+    private lateinit var playButton2: ImageView
+    private lateinit var playButton3: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
