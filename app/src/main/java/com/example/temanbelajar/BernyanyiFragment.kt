f048faac4db8ab8bff06276028bc0b57f783edf1
Hikmatun Nazilah <hikmatun_nazilah@teknokrat.ac.id>
2025-06-23 20:38:28 +0700

    Membuat fungsi yang di panggil saat fragment pertama kali di buat pada BernyanyiFragment.kt

--- app/src/main/java/com/example/temanbelajar/BernyanyiFragment.kt
+++ app/src/main/java/com/example/temanbelajar/BernyanyiFragment.kt
@@ -28,19 +28,11 @@ class BernyanyiFragment : Fragment() {
     private lateinit var playButton2: ImageView
     private lateinit var playButton3: ImageView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bernyanyi, container, false)
-    }
 
     companion object {
         /**
