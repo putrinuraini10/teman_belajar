1d29a274576e1bf2df0d55e6e60f8b706a40038a
Putri Nuraini <putri_nuraini@teknokrat.ac.id>
2025-06-28 22:06:08 +0700

    Cleaned up repository for new project

--- app/src/main/java/com/example/temanbelajar/Question.kt
+++
@@ -1,9 +0,0 @@
-package com.example.temanbelajar
-
-data class Question(
-    val questionText: String,
-    val options: List<String>,
-    val correctAnswerIndex: Int,
-    val imageResId: Int? = null,
-    val audioResId: Int? = null
-)
